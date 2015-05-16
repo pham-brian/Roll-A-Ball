b8f0c8c2f9792ee860e77cabb6edae929805d9ac
Brian <hieubrianpham@gmail.com>
2015-05-16 09:37:52 +0700

    unity game demo
    unity game demo

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		winText.text = "";
+		setCountText ();
+	}
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal"); 
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			setCountText ();
+		}
+	}
+
+	void setCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 8) {
+			winText.text = "You win!";		
+		}
+	}
+}
